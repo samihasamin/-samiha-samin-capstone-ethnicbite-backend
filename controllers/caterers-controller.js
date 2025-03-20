27a99ec1690ebf37e47cfee6fff8dccb4d756796
samihasamin <samihasamin@hotmail.com>
2025-03-20 04:12:42 -0400

    Modify getCatererById function to include the meal seeker's name when fetching reviews

--- controllers/caterers-controller.js
+++ controllers/caterers-controller.js
@@ -37,11 +37,17 @@ const getCatererById = async (req, res) => {
 
     const meals = await knex("meals")
       .where("caterer_id", id)
-      .select("name", "description", "price", "image_url");
+      .select("id", "name", "description", "price", "image_url");
 
     const reviews = await knex("reviews")
-      .where("caterer_id", id)
-      .select("meal_seeker_id", "rating", "review", "created_at");
+      .join("meal_seekers", "reviews.meal_seeker_id", "meal_seekers.id")
+      .where("reviews.caterer_id", id)
+      .select(
+        "meal_seekers.name as meal_seeker_name",
+        "reviews.rating",
+        "reviews.review",
+        "reviews.created_at"
+      );
 
     const aboutMe = await knex("about_me")
       .where("caterer_id", id)
